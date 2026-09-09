94f0b108351e16b9078a100ccd736466286812b8
chronjean <megachronoje@gmail.com>
2026-09-09 14:48:40 -0400

    Team Activity W02: Step 5 completed

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,14 @@
 import {
   getLocalStorage,
   setLocalStorage,
+  getParam,
 } from "./utils.mjs";
 
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
+const productId = getParam("product");
+console.log(dataSource.findProductById(productId));
 
 
 function addProductToCart(product) {
