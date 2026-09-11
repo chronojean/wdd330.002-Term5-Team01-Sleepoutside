06e6ee73e2c45505026314b17a80e8e0db146a56
chronojean <megachronoje@gmail.com>
2026-09-11 12:30:42 -0400

    Step 7 ongoing

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -6,11 +6,38 @@ export default class ProductDetails {
     this.product = {};
     this.dataSource = dataSource;
   }
-  init() {}
+  async init() {
+    // 1. pide los datos del producto usando el dataSource
+    this.product = await this.dataSource.findProductById(this.productId);
+
+    // 2. renderiza el HTML del producto (esto lo construimos en el próximo paso)
+    this.renderProductDetails();
+
+    // 3. engancha el botón "Add to Cart"
+    document
+      .getElementById("addToCart")
+      .addEventListener("click", this.addProductToCart.bind(this));
+  }
   addProductToCart() {
-    const cartItems = getLocalStorage("so-cart");
+    const cartItems = getLocalStorage("so-cart") || [];
     cartItems.push(this.product);
     setLocalStorage("so-cart", cartItems);
   }
-  renderProductDetails() {}
+  renderProductDetails() {
+    this.productDetailsTemplate(this.product);
+  }
+  productDetailsTemplate(product) {
+    document.querySelector('h2').textContent = product.Brand.Name;
+    document.querySelector('h3').textContent = product.NameWithoutBrand;
+
+    const productImage = document.getElementById('productImage');
+    productImage.src = product.Image;
+    productImage.alt = product.NameWithoutBrand;
+
+    document.getElementById('productPrice').textContent = product.FinalPrice;
+    document.getElementById('productColor').textContent = product.Colors[0].ColorName;
+    document.getElementById('productDesc').innerHTML = product.DescriptionHtmlSimple;
+
+    document.getElementById('addToCart').dataset.id = product.Id;
+  }
 }
