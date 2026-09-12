2e8b2718c5d5edfb061a6d049fd77a3c46979211
josheeen <therealjosh26@gmail.com>
2026-09-12 14:43:49 +0800

    team act week 2.

--- src/js/product.js
+++ src/js/product.js
@@ -2,9 +2,29 @@ import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
+// 1. Create the data source
 const dataSource = new ProductData("tents");
-const productId = getParam("product");
 
-const product = new ProductDetails(productId, dataSource);
+// 2. Get the product ID from the URL
+const productID = getParam("product");
 
+// 3. Create the ProductDetails instance and initialize it
+const product = new ProductDetails(productID, dataSource);
 product.init();
+
+// function addProductToCart(product) {
+//   const cartItems = getLocalStorage("so-cart") || []; // get cart array of items from local storage if null set to empty array
+//   cartItems.push(product);
+//   setLocalStorage("so-cart", cartItems);
+// }
+
+// // add to cart button event handler
+// async function addToCartHandler(e) {
+//   const product = await dataSource.findProductById(e.target.dataset.id);
+//   addProductToCart(product);
+// }
+
+// // add listener to Add to Cart button
+// document
+//   .getElementById("addToCart")
+//   .addEventListener("click", addToCartHandler);
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -7,7 +7,7 @@ export function qs(selector, parent = document) {
 
 // retrieve data from localstorage
 export function getLocalStorage(key) {
-  return JSON.parse(localStorage.getItem(key) || "[]");
+  return JSON.parse(localStorage.getItem(key));
 }
 // save data to local storage
 export function setLocalStorage(key, data) {
@@ -22,6 +22,8 @@ export function setClick(selector, callback) {
   qs(selector).addEventListener("click", callback);
 }
 
+// NEW FUNCTION FOR TEAM ACTIVITY:
+// get the product id from the query string
 export function getParam(param) {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
